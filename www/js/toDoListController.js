321ecfe516aafe6ebbd6ca1ec266ce0645ebbc39
giraudier pierre <giraudier.pierre@gmail.com>
2018-06-15 16:40:34 +0200

    create get task content method on blur

--- www/js/toDoListController.js
+++ www/js/toDoListController.js
@@ -47,10 +47,16 @@ angular.module('starter.controllers')
 
             promise.then(function(data) {
                 //Success
-                console.log(data);
             }, function() {
                 //Error
             });
+        }
+
+        $scope.getTaskContent = function(taskContent) {
+
+            var data = {
+                'content': taskContent,
+            }
         }
 
         $scope.getTags = function() {
